c385bc14fc853c82dea29d775ab110e569045a7f
keyplay <houyubo1993@foxmail.com>
2026-01-19 10:59:12 +0800

    Delete NCMAPSS_F class and related settings
    Removed the NCMAPSS_F class and its associated configurations.

--- configs/data_model_configs.py
+++ configs/data_model_configs.py
@@ -82,43 +82,7 @@ class NCMAPSS():
         self.hidden_dim = 500
         self.DSKN_disc_hid = 128
 
-class NCMAPSS_F():
-    def __init__(self):
-        super(NCMAPSS_F, self)
-        self.scenarios = [("flight1", "flight2"), ("flight1", "flight3"), ("flight2", "flight1"), ("flight2", "flight3"), ("flight3", "flight1"), ("flight3", "flight2")]
-        self.sequence_len = 50
-        self.max_rul = 88
-        self.shuffle = True
-        self.drop_last = True
-        self.normalize = False
 
-        # model configs
-        self.input_channels = 20
-        self.kernel_size = 3
-        self.stride = 1
-        self.dropout = 0.5
-        self.num_classes = 6
-
-        # CNN and RESNET features
-        self.mid_channels = 20
-        self.final_out_channels = 20
-        self.features_len = 1
-
-        # TCN features
-        self.tcn_layers = [75, 150]
-        self.tcn_final_out_channles = self.tcn_layers[-1]
-        self.tcn_kernel_size = 17
-        self.tcn_dropout = 0.0
-
-        # lstm features
-        self.lstm_hid = 128
-        self.lstm_n_layers = 1
-        self.lstm_bid = False
-
-        # discriminator
-        self.disc_hid_dim = 64
-        self.hidden_dim = 500
-        self.DSKN_disc_hid = 128
 
 
         
